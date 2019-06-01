c06931ce7d81a42d31698eafda5e3420ac9e3ece
Tim Jester-Pfadt <tim.jester-pfadt@merckgroup.com>
2019-06-01 19:21:04 +0200

    Add meta data for hex.pm

--- mix.exs
+++ mix.exs
@@ -6,8 +6,22 @@ defmodule Libp2p.MixProject do
       app: :libp2p,
       version: "0.1.0",
       elixir: "~> 1.8",
+      description: description(),
+      package: package(),
       start_permanent: Mix.env() == :prod,
+      source_url: "https://github.com/timjp87/elixir-libp2p",
       deps: deps()
+    ]
+  end
+
+  defp description() do
+    "This package provides bindings for the go-libp2p-daemon, which needs to be installed and launched seperately."
+  end
+
+  defp package() do
+    [
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/timjp87/elixir-libp2p"}
     ]
   end
 
